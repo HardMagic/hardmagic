b1b87e0899a4550370f0cd03503171f326b3a433
Matthew Hackney <matthew@taomgt.com>
2020-04-26 10:35:52 -0400

    initial

---
+++ wp-content/plugins/leaflet-maps-marker/leaflet-georss7cfb.php
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:georss="http://www.georss.org/georss" xmlns:gml="http://www.opengis.net/gml" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/">
+<channel>
+<atom:link href="https://hardmagic.com/wp-content/plugins/leaflet-maps-marker/leaflet-georss.php?layer=all" rel="self" type="application/rss+xml" />
+<link>https://hardmagic.com/wp-content/plugins/leaflet-maps-marker/leaflet-fullscreen.php?layer=0</link>
+<title>HardMagic - maps</title>
+<lastBuildDate>Thu, 01 Jan 1970 12:01:00 +2000</lastBuildDate>
+<generator>www.mapsmarker.com</generator>
+<description>GeoRSS-feed created with Maps Marker Pro (www.mapsmarker.com)</description>
+</channel>
+</rss>
+<!--
+Performance optimized by W3 Total Cache. Learn more: https://www.boldgrid.com/w3-total-cache/
+
+Object Caching 0/147 objects using apc
+Page Caching using apc (SSL caching disabled) 
+Minified using apc
+Database Caching using apc (Request-wide modification query)
+
+Served from: *.airikai.com @ 2020-04-26 10:15:08 by W3 Total Cache
+-->
