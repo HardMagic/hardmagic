8cb8f9769f9bc164c6a91de93d6c1aea15400fb5
Björn Mage <bjorn.mage@focusapiary.com>
2026-08-12 14:29:11 -0400

    Support GitLab Pages project base path

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://hardmagic.com',
+  base: process.env.CI ? '/hardmagic' : '/',
   output: 'static',
   trailingSlash: 'always',
   compressHTML: 'jsx',
